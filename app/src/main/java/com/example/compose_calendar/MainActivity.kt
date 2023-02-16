9548d77d8955504531689cd7af2bef15166c82ec
Furkan Kaymaz <furkannkaymaz@gmail.com>
2023-02-16 13:01:41 +0300

    v1.0 release

--- app/src/main/java/com/example/compose_calendar/MainActivity.kt
+++ app/src/main/java/com/example/compose_calendar/MainActivity.kt
@@ -1,46 +1,31 @@
 package com.example.compose_calendar
 
-import android.content.Context
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
-import android.widget.TextClock
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.selection.selectable
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.capitalize
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.example.compose_calendar.ui.theme.APP_BG
 import com.example.compose_calendar.ui.theme.ComposecalendarTheme
-import java.text.DateFormatSymbols
 import java.time.DateTimeException
-import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.Month
-import java.time.Year
 import java.util.*
-import kotlin.time.Duration.Companion.days
 
 class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
@@ -79,15 +64,15 @@ fun SimpleCalendar() {
 
     val dateList = remember { mutableStateListOf<LocalDate>() }
 
-    Column(Modifier.verticalScroll(rememberScrollState())) {
+    Column(Modifier.verticalScroll(rememberScrollState()).background(APP_BG)) {
         Row {
             Row(modifier = Modifier.padding(16.dp)) {
-                Text("Select a year:", Modifier.clickable { expanded = true })
-                Spacer(modifier = Modifier.width(16.dp))
+                Text("Select a year:", Modifier.clickable { expanded = true },fontWeight = FontWeight.Bold)
+                Spacer(modifier = Modifier.width(8.dp))
                 Text(chosenYear.toString(), Modifier.clickable { expanded = true })
             }
             DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
-                for (year in 1960..2030) {
+                for (year in 2023 downTo 1960) {
                     DropdownMenuItem(onClick = {
                         selectedYear = year
                         expanded = false
@@ -100,8 +85,8 @@ fun SimpleCalendar() {
         }
         Row {
             Row(modifier = Modifier.padding(16.dp)) {
-                Text("Select a month:", Modifier.clickable { expandedMonth = true })
-                Spacer(modifier = Modifier.width(16.dp))
+                Text("Select a month:", Modifier.clickable { expandedMonth = true },fontWeight = FontWeight.Bold)
+                Spacer(modifier = Modifier.width(8.dp))
                 Text(chosenMonth.toString(), Modifier.clickable { expandedMonth = true })
             }
 
@@ -117,68 +102,85 @@ fun SimpleCalendar() {
                 }
             }
         }
-        Spacer(modifier = Modifier.height(32.dp))
-        Row(Modifier.fillMaxWidth()) {
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .border(BorderStroke(2.dp, Color.Black))) {
             val weekdays = listOf("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
             weekdays.forEach {
                 Text(
                     text = it,
                     textAlign = TextAlign.Center,
                     fontWeight = FontWeight.Bold,
-                    modifier = Modifier.weight(1f)
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(4.dp)
                 )
             }
         }
         val firstDayOfMonth = LocalDate.of(selectedYear, selectedMonth, 1)
         val firstDayOfWeek = firstDayOfMonth.dayOfWeek.value
-        Spacer(modifier = Modifier.height(8.dp))
-        LazyVerticalGrid(columns = GridCells.Fixed(7), modifier = Modifier.height(250.dp)) {
-            val emptyCells = List(firstDayOfWeek - 1) { null }
-            val daysInMonth = firstDayOfMonth.lengthOfMonth()
-            items(emptyCells.size + daysInMonth + 2) { dayOfMonth ->
-                val day = dayOfMonth - emptyCells.size + 1
-                val textColor = if ((day + firstDayOfWeek - 3) % 7 == 4 || (day + firstDayOfWeek - 3) % 7 == 5) Color.Red else Color.Black
-                var backgroundColor = Color.Transparent
 
-                if (day >= 1 || day > daysInMonth){
-                    val currentDate = try {
-                        LocalDate.of(selectedYear, selectedMonth, day)
-                    }catch (e : DateTimeException){
-                        null
+        Box(Modifier.padding(16.dp).border(BorderStroke(1.dp, Color.Black))) {
+            LazyVerticalGrid(columns = GridCells.Fixed(7), modifier = Modifier
+                .height(195.dp)
+                .padding(0.dp,8.dp)
+                ) {
+                val emptyCells = List(firstDayOfWeek - 1) { null }
+                val daysInMonth = firstDayOfMonth.lengthOfMonth()
+                items(emptyCells.size + daysInMonth + 2) { dayOfMonth ->
+                    val day = dayOfMonth - emptyCells.size + 1
+                    val textColor =
+                        if ((day + firstDayOfWeek - 3) % 7 == 4 || (day + firstDayOfWeek - 3) % 7 == 5) Color.Red else Color.Black
+                    var backgroundColor = Color.Transparent
+
+                    if (day >= 1 || day > daysInMonth) {
+                        val currentDate = try {
+                            LocalDate.of(selectedYear, selectedMonth, day)
+                        } catch (e: DateTimeException) {
+                            null
+                        }
+                        backgroundColor =
+                            if (dateList.contains(currentDate)) Color.Gray else Color.Transparent
                     }
-                    backgroundColor = if (dateList.contains(currentDate)) Color.Cyan else Color.Transparent
+                    Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+                        Text(
+                            text = if (day <= 0 || day > daysInMonth) " " else day.toString(),
+                            textAlign = TextAlign.Center,
+                            color = textColor,
+                            modifier = Modifier
+                                .background(backgroundColor)
+                                .width(32.dp)
+                                .height(32.dp)
+                                .clickable(
+                                    enabled = !(day <= 0 || day > daysInMonth)
+                                ) {
+
+                                    sendDate = LocalDate.of(selectedYear, selectedMonth, day)
+                                    onDateClick(sendDate, dateList)
+                                },
+                        )
+                    }
+
+
                 }
-
-                Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
-                    Text(
-                        text = if (day <= 0 || day > daysInMonth) " " else day.toString(),
-                        textAlign = TextAlign.Center,
-                        color = textColor,
-                        modifier = Modifier
-                            .background(backgroundColor)
-                            .width(32.dp)
-                            .height(32.dp)
-                            .clickable(
-                                enabled = !(day <= 0 || day > daysInMonth)
-                            ) {
-
-                                sendDate = LocalDate.of(selectedYear, selectedMonth, day)
-                                onDateClick(sendDate, dateList)
-                            },
-                    )
-                }
-
             }
         }
+        Text("Chosen Dates:", Modifier.padding(16.dp),fontWeight = FontWeight.Bold)
         LazyColumn(
             Modifier
-                .height(250.dp)
-                .fillMaxWidth()
+                .height(350.dp)
                 .padding(16.dp)
+                .fillMaxWidth()
         ) {
             items(dateList.size) { date ->
-                Text(text = dateList.get(date).toString())
-                Spacer(modifier = Modifier.height(5.dp))
+                Box(modifier = Modifier
+                    .padding(8.dp)
+                    .border(BorderStroke(1.dp, Color.Black)) ){
+                    Text(text = dateList.get(date).toString(), modifier = Modifier.padding(8.dp))
+                    Spacer(modifier = Modifier.height(5.dp))
+                }
             }
         }
 
